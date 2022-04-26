33a9194059df0910aa363841a09df556ed581932
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-04-26 14:58:34 +0200

    Added Base 2d Car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelJoint2D frontWheel;
+    public WheelJoint2D rearWheel;
+    JointMotor2D front;
+    JointMotor2D back;
+    public float speedForward,speedBackward;
+    public float torqueForward, torqueBackward;
+    public bool useFront = true, useRear = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            if (useFront)
+            {
+                front.motorSpeed = speedForward;
+                front.maxMotorTorque = torqueForward;
+                frontWheel.motor = front;
+            }
+            if (useRear)
+            {
+                back.motorSpeed = speedForward;
+                back.maxMotorTorque = torqueForward;
+                rearWheel.motor = back;
+            } 
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (useFront)
+            {
+                front.motorSpeed = speedBackward;
+                front.maxMotorTorque = torqueBackward;
+                frontWheel.motor = front;
+            }
+            if (useRear)
+            {
+                back.motorSpeed = speedBackward;
+                back.maxMotorTorque = torqueBackward;
+                rearWheel.motor = back;
+            }
+        }
+        else
+        {
+            rearWheel.useMotor = false;
+            frontWheel.useMotor = false;
+        }
+    }
+}
